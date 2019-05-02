a0506742c67a78b330460aef3a2177dfea206c14
Romain Bourjot <romain.bourjot@gmail.com>
2019-05-02 15:43:48 +0200

    ALIVE

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,9 +1,46 @@
 // @noflow
 
 import {expect} from 'chai';
+import {isError, success, fail} from 'reither';
 
 describe('reither', () => {
-	it('Should do stuf', () => {
-		expect(true).to.equal(true);
+	it('Should create a fail', () => {
+		const expectedValue = Symbol();
+
+		const result = fail(expectedValue);
+
+		const expected = {
+			isError: true,
+			error: expectedValue,
+			value: undefined
+		};
+
+		const actual = {
+			isError: isError(result),
+			error: result.error,
+			value: result.value
+		};
+
+		expect(expected).to.deep.equal(actual);
+	});
+
+	it('Should create a successs', () => {
+		const expectedValue = Symbol();
+
+		const result = success(expectedValue);
+
+		const expected = {
+			isError: false,
+			error: undefined,
+			value: expectedValue
+		};
+
+		const actual = {
+			isError: isError(result),
+			error: result.error,
+			value: result.value
+		};
+
+		expect(expected).to.deep.equal(actual);
 	});
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 // @flow
 
-console.log('Hello :');
+export type Either<T, U> = $ReadOnly<
+	| {|isError: true, error: U|}
+	| {|isError: false, value: T|}
+>;
+
+export function fail<T>(error: T): Either<empty, T> {
+	return {isError: true, error};
+}
+
+export function success<T>(value: T): Either<T, empty> {
+	return {isError: false, value};
+}
+
+export function isError(either: Either<any, any>): boolean %checks {
+	return either.isError;
+}
